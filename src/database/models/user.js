20156cf69231eb0d29af543c563ada65b36d77d0
pandu926 <63030609+pandu926@users.noreply.github.com>
2023-10-14 08:28:34 +0700

    pandu

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -13,13 +11,19 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  user.init({
-    nim: DataTypes.INTEGER,
-    nama: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+  user.init(
+    {
+      nim: DataTypes.INTEGER,
+      nama: DataTypes.STRING,
+      password: DataTypes.STRING,
+      email: DataTypes.STRING,
+      semester: DataTypes.INTEGER,
+      hp: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
   return user;
 };
